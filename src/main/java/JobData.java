9ebd76b30767aea510556269778ffeba96076625
jasminbecic <becicjasmin1@gmail.com>
2022-01-10 19:54:44 -0600

    fixed minor bugs

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -74,9 +74,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column).toLowerCase(Locale.ROOT);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -103,7 +103,7 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
             for (Map.Entry<String, String> r : row.entrySet()) {
-                if (r.getValue().toLowerCase(Locale.ROOT).contains(value.toLowerCase(Locale.ROOT))) {
+                if (r.getValue().toLowerCase().contains(value.toLowerCase())) {
                     jobs.add(row);
                     break;
                 }
